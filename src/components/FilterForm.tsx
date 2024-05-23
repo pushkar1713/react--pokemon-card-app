113d01f5e75dea234c258fe4877c3260c4850915
Vincent Chaussepied <vincent.chaussepied@gmail.com>
2024-05-23 08:08:32 +0100

    added mobile responsiveness

--- src/components/FilterForm.tsx
+++ src/components/FilterForm.tsx
@@ -4,71 +4,86 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFilter } from "@fortawesome/free-solid-svg-icons";
 
 type FilterFormProps = {
-  onFilterChange: (filters: { type: string; rarity: string }) => void;
+  onFilterChange: (filters: {
+    type: string;
+    rarity: string;
+    supertype: string;
+  }) => void;
 };
 
 export const FilterForm = ({ onFilterChange }: FilterFormProps) => {
-  const [type, setType] = useState("");
-  const [rarity, setRarity] = useState("");
+  const [type, setType] = useState<string>("");
+  const [rarity, setRarity] = useState<string>("");
+  const [supertype, setSupertype] = useState<string>("");
+  const [menuOpen, setMenuOpen] = useState<boolean>(false);
 
   useEffect(() => {
-    onFilterChange({ type, rarity });
-  }, [type, rarity, onFilterChange]);
+    onFilterChange({ type, rarity, supertype });
+  }, [type, rarity, supertype, onFilterChange]);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
 
   return (
-    <form className={styles.filterForm}>
-      <div className={styles.filterTitle}>
-        <FontAwesomeIcon id={styles.filterIcon} icon={faFilter} />
-        <h2>Filter</h2>
-      </div>
-      <label htmlFor="type">By Type</label>
-      <select
-        name="type"
-        id="type"
-        value={type}
-        onChange={(e) => setType(e.target.value)}
-      >
-        <option value="">Select Type</option>
-        <option value="fire">Fire</option>
-        <option value="water">Water</option>
-        <option value="grass">Grass</option>
-        <option value="colorless">Colorless</option>
-        <option value="darkness">Darkness</option>
-        <option value="dragon">Dragon</option>
-        <option value="fairy">Fairy</option>
-        <option value="fighting">Fighting</option>
-        <option value="lightning">Lightning</option>
-        <option value="metal">Metal</option>
-        <option value="psychic">Psychic</option>
-      </select>
+    <div>
+      <button className={styles.hamburger} onClick={toggleMenu}>
+        <FontAwesomeIcon icon={faFilter} className={styles.filterIcon} />
+      </button>
+      <form className={`${styles.filterForm} ${menuOpen ? styles.show : ""}`}>
+        <div className={styles.filterTitle}>
+          <FontAwesomeIcon id={styles.filterIcon} icon={faFilter} />
+          <h2>Filter</h2>
+        </div>
+        <label htmlFor="type">By Type</label>
+        <select
+          name="type"
+          id="type"
+          value={type}
+          onChange={(e) => setType(e.target.value)}
+        >
+          <option value="">Select Type</option>
+          <option value="fire">Fire</option>
+          <option value="water">Water</option>
+          <option value="grass">Grass</option>
+          <option value="colorless">Colorless</option>
+          <option value="darkness">Darkness</option>
+          <option value="dragon">Dragon</option>
+          <option value="fairy">Fairy</option>
+          <option value="fighting">Fighting</option>
+          <option value="lightning">Lightning</option>
+          <option value="metal">Metal</option>
+          <option value="psychic">Psychic</option>
+        </select>
 
-      <label htmlFor="rare">By Rarity</label>
-      <select
-        name="rare"
-        id="rare"
-        value={rarity}
-        onChange={(e) => setRarity(e.target.value)}
-      >
-        <option value="">Select Rarity</option>
-        <option value="Common">Common</option>
-        <option value="Uncommon">Uncommon</option>
-        <option value="Rare Holo">Rare Holo</option>
-        <option value="Rare Ultra">Rare Ultra</option>
-        <option value="Shiny Rare">Shiny Rare</option>
-      </select>
+        <label htmlFor="rarity">By Rarity</label>
+        <select
+          name="rarity"
+          id="rarity"
+          value={rarity}
+          onChange={(e) => setRarity(e.target.value)}
+        >
+          <option value="">Select Rarity</option>
+          <option value="Common">Common</option>
+          <option value="Uncommon">Uncommon</option>
+          <option value="Rare Holo">Rare Holo</option>
+          <option value="Rare Ultra">Rare Ultra</option>
+          <option value="Shiny Rare">Shiny Rare</option>
+        </select>
 
-      <label htmlFor="supertype">By Supertype</label>
-      <select
-        name="rare"
-        id="rare"
-        value={rarity}
-        onChange={(e) => setRarity(e.target.value)}
-      >
-        <option value="">Select Supertype</option>
-        <option value="Energy">Energy</option>
-        <option value="Pokémon">Pokémon</option>
-        <option value="Trainer">Trainer</option>
-      </select>
-    </form>
+        <label htmlFor="supertype">By Supertype</label>
+        <select
+          name="supertype"
+          id="supertype"
+          value={supertype}
+          onChange={(e) => setSupertype(e.target.value)}
+        >
+          <option value="">Select Supertype</option>
+          <option value="Energy">Energy</option>
+          <option value="Pokémon">Pokémon</option>
+          <option value="Trainer">Trainer</option>
+        </select>
+      </form>
+    </div>
   );
 };
